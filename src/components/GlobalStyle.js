4a7b8a1f84c0b7ccd4c2bb9a29e71093fd90f533
InnaKara <innatvinna@gmail.com>
2023-11-05 02:03:54 +0100

    fin

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -2,6 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
+
+h1 {
+  font-size: 25px;
+  text-align: center;
+}
+h2{
+  font-size: 25px;
+  margin: 35px auto 5px;
+  text-align: center;
+};
+h3{
+    font-size: 20px;
+  text-align: center;
+  margin: 5px auto;
+};
+
   body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -21,4 +37,8 @@ export const GlobalStyle = createGlobalStyle`
     max-width: 100%;
     object-fit: cover;
   }
+
+
+
+
 `;
